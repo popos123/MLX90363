29a75c1d07e8bf2df115203e0bcc452590db0e1b
popos123 <43697578+popos123@users.noreply.github.com>
2021-10-15 17:19:47 +0200

    Add files via upload

---
+++ with_delay/example.ino
@@ -0,0 +1,18 @@
+//#define Software // software if definde or hardware SPI if not
+
+#include <Arduino.h>
+#include "MLX90363.h"
+
+Magnet MLX90363;
+
+void setup() {
+  MLX90363.begin(16, 14, 15, 10); // MOSI, MISO, SCK, CS
+  //MLX90363->begin(PA7, PA6, PA5, PA4); // for Blue Pill
+  Serial.begin(115200);
+}
+
+void loop() {
+  MLX90363.ReadData();
+  Serial.println("Angle_(Lsb): " + String(MLX90363.AngleLSB()));
+  delay(1000);
+}
