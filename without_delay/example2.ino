85dd7b93e0f969dcb6126284dd0e91c3b8626f54
popos123 <43697578+popos123@users.noreply.github.com>
2021-10-15 20:59:04 +0200

    Add files via upload

---
+++ without_delay/example2.ino
@@ -0,0 +1,21 @@
+#include <Arduino.h>
+#include "MLX90363.h"
+
+Magnet MLX90363;
+
+uint32_t last_cnt = 0;
+
+void setup() {
+  MLX90363.begin(16, 14, 15, 10); // MOSI, MISO, SCK, CS
+  Serial.begin(500000);
+}
+
+void loop() {
+  MLX90363.ReadData(); // read data
+  // print data
+  uint32_t current_cnt = millis();
+  if (current_cnt - last_cnt >= 100) { // print every 100ms
+    last_cnt = current_cnt;
+    Serial.println("Angle_(LSB): " + String(MLX90363.Angle_LSB));
+  }
+}
